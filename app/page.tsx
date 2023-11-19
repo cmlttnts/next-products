74688413e48f8fdd65bf6dab83989151da88ace6
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2023-11-19 16:28:37 +0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main >
+      <Image src="/public/mobile-image.png" alt="Picture of the author" />
       
     </main>
   )
